46a52e2e02876091089b3da37adda41940a027c9
Troy Denney <tvdenney@gmail.com>
2016-10-12 10:37:04 -0400

    working on string

---
+++ datatypes.js
@@ -0,0 +1,36 @@
+//String
+// var word = '     Lorem shaslcbhjca dojncbv,jshdbvdc aschnfdksjfbdsj,dsbjhds djsfkdsjfkdsj     '
+
+// console.log(word.length) // <-- property (a variable essentially)
+// console.log(word.toUpperCase())  // <-- method (a function essentially)
+// console.log(word.toLowerCase())
+// word = word.toUpperCase()
+// word = word.trim()
+// word = word.toLowerCase()
+// console.log(word)
+
+// word = word.toUpperCase().trim().toLowerCase()
+// console.log(word)
+
+var word = 'NBA,NFL,MLB'
+
+//word = word.repeat(5)
+// word = 'Pizza Is Amazing'.substr(9, 7)
+// word = 'Pizza Is Amazing'.slice(9,16)
+//word = word.split(',')
+console.log(word)
+
+//Number
+//Date
+//Math
+//Object
+//Array
+
+var name = String('Tom')
+var name2 = 'Tom'
+
+//console.log(name ===name2)
+
+
+var number = Number(1) //=== 1
+var boolean = Boolean(true) //=== true
